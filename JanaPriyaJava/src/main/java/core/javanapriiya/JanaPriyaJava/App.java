ade3309a7f302f12fe09233c52be420a223a50d2
razzaksr <razzaksr@gmail.com>
2021-03-19 10:24:17 +0530

    First commit

---
+++ JanaPriyaJava/src/main/java/core/javanapriiya/JanaPriyaJava/App.java
@@ -0,0 +1,13 @@
+package core.javanapriiya.JanaPriyaJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Zealous Welcomes you!!!!!!!" );
+    }
+}
